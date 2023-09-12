5bbec22ea93835e46a746d95c23c0b5f86fc63ce
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-12 14:16:15 +0300

    Back <> JWT Bug Fix

--- talent-server/routes/api.php
+++ talent-server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UploadFileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
 Route::post('/upload-file', [UploadFileController::class, 'uploadFile']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
