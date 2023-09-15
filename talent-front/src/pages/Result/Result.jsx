22585c8044e3f7dc575bb70fbe81669f409e92f5
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-15 14:31:53 +0300

    Front <> Result Navigation

--- talent-front/src/pages/Result/Result.jsx
+++ talent-front/src/pages/Result/Result.jsx
@@ -1,16 +1,33 @@
 
 import React from 'react';
 import './style.css';
+import Navbar from '../../components/Navbar/Navbar';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Result = ({ content }) => {
+  const navigation = useNavigate();
+  const handlefinishbtn = () => {
+
+    navigation('/')
+
+  }
+
   return (
     <>
+      <Navbar selecteditem='Add Candidate' />
       <div className='hcontainer'>
         <h1>AI Result</h1>
       </div>
       <div className="result-container">
         <textarea className="result-textarea" value={"loremhadsjkldahaslkjhlakjhdblsakjbflaskdjbflkjasdbclkjasdbclkjasdbclkjasbdclkjasblkjcbasdkljcbasdkljcbklasdcbsadkjcbsadkjlcbsladjkbclkasdjbcklasjdbcklasdjbclksadjbckljasdbckljasdbclkjsadb"} readOnly />
+        <span className="finishcontainer">
+          <button className="finishairesult" onClick={handlefinishbtn}>Finish</button>
+        </span>
       </div>
+
+
     </>
   );
 };
