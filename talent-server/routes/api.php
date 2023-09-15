19264d877651717360e33cf8831b79af4d8d0a5a
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-15 15:57:37 +0300

    Back <> Read TXT API

--- talent-server/routes/api.php
+++ talent-server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\UploadFileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,8 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
+Route::get('/read-text-file', [FileController::class, 'readTextFile']);
+
 Route::post('/upload-file', [UploadFileController::class, 'uploadFile']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
