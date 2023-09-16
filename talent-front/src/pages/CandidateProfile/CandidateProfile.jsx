830166ec3de782d9fa4a5633f273c029b2305861
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-16 22:42:53 +0300

    Front <> Footer And Loading CandidateProfile

--- talent-front/src/pages/CandidateProfile/CandidateProfile.jsx
+++ talent-front/src/pages/CandidateProfile/CandidateProfile.jsx
@@ -7,8 +7,12 @@ import makeAnimated from "react-select/animated";
 import { sendRequest } from "../../core/config/request";
 import { requestMethods } from "../../core/enums/requestMethods";
 import { localStorageAction } from "../../core/config/localstorage";
+import Footer from '../../components/Footer/Footer';
+import Loading from '../Loading/Loading';
 const CandidateProfile = () => {
   const animatedComponents = makeAnimated();
+  const [loading, setLoading] = useState(false);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [candidateprofile, setCandidateProfile] = useState({
     full_name: '',
@@ -70,21 +74,18 @@ const CandidateProfile = () => {
   };
   const [imagePreview, setImagePreview] = useState(null);
 
-  // Function to handle image selection
   const handlecandidateprofile = (e) => {
     const { name, value, type } = e.target;
 
     if (type === 'file') {
-      // Create a FormData object and append the file to it
       const formData = new FormData();
       formData.append(name, e.target.files[0]);
 
       setCandidateProfile({
         ...candidateprofile,
-        [name]: e.target.files[0], // Store the FormData object in the payload
+        [name]: e.target.files[0],
       });
     } else {
-      // Handle other input fields
       setCandidateProfile({
         ...candidateprofile,
         [name]: value,
@@ -101,18 +102,18 @@ const CandidateProfile = () => {
     formData.append('phone', candidateprofile.phone);
     formData.append('position', candidateprofile.position);
     formData.append('image', candidateprofile.image);
-
-    console.log('Candidate Profile Data:', formData);
-
+    setLoading(true);
     try {
       const response = await sendRequest({
         method: requestMethods.POST,
         route: '/addcandidate',
         body: formData,
       });
-      console.log(response);
-      navigation('/result')
+      setLoading(false);
+
+      navigation('/upload/' + response['candidate_id'])
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   };
@@ -138,68 +139,76 @@ const CandidateProfile = () => {
 
   return (
     <>
-      <Navbar selecteditem='Add Candidate' />
-      <form action="#" method="post" id="signup">
-        <span className='flex'>
-          <h1>Candidate Profile</h1>
-          <i className="fas fa-user" style={{ fontSize: '40px', marginLeft: '20px' }}></i>
-        </span>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
 
-        <fieldset className='fieldset'>
-          <label>
-            Full Name<br />
-            <input type="text" name="full_name" placeholder="John Doe" onChange={handlecandidateprofile} required /><br />
-          </label>
-          <label>
-            Date of Birth<br />
-            <input
-              type="date"
-              name="date_of_birth "
-              value={selectedDate}
-              onChange={handleDateChange}
-              required
-            /><br />
-          </label>
-          <Select
-            value={selectedOptions}
-            components={animatedComponents}
-            options={options}
-            onChange={handleSelect}
-            styles={customStyles}
-            onMenuOpen={() => setIsMenuOpen(true)}
-            onMenuClose={() => setIsMenuOpen(false)}
-            onInputChange={(inputValue) => {
-              if (!inputValue) {
-                setIsMenuOpen(true);
-              }
-            }}
-            className="select"
-            isClearable={true}
-            isSearchable={false}
-            isDisabled={false}
-            isLoading={false}
-            isRtl={false}
-            menuIsOpen={isMenuOpen}
-            placeholder="Select the job position."
-          />
-          <label >
-            Email<br />
-            <input type="email" name="email" placeholder="johndoe@example.com" onChange={handlecandidateprofile} required /><br />
-          </label>
-          <label >
-            Phone<br />
-            <input type="phone" name="phone" placeholder="76968647" onChange={handlecandidateprofile} required /><br />
-          </label>
+          <Navbar selecteditem='Add Candidate' />
+          <form action="#" method="post" id="signup">
+            <span className='flex'>
+              <h1>Candidate Profile</h1>
+              <i className="fas fa-user" style={{ fontSize: '40px', marginLeft: '20px' }}></i>
+            </span>
 
-          <label >
-            Profile Picture<br />
-            <input type="file" name="image" onChange={handlecandidateprofile} accept="image/*" /><br />
-          </label>
-        </fieldset>
-        <footer>
-          <button type="submit" onClick={handlecreatebtn}>Create</button>
-        </footer>
-      </form >
+            <fieldset className='fieldset'>
+              <label>
+                Full Name<br />
+                <input type="text" name="full_name" placeholder="John Doe" onChange={handlecandidateprofile} required /><br />
+              </label>
+              <label>
+                Date of Birth<br />
+                <input
+                  type="date"
+                  name="date_of_birth "
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  required
+                /><br />
+              </label>
+              <Select
+                value={selectedOptions}
+                components={animatedComponents}
+                options={options}
+                onChange={handleSelect}
+                styles={customStyles}
+                onMenuOpen={() => setIsMenuOpen(true)}
+                onMenuClose={() => setIsMenuOpen(false)}
+                onInputChange={(inputValue) => {
+                  if (!inputValue) {
+                    setIsMenuOpen(true);
+                  }
+                }}
+                className="select"
+                isClearable={false}
+                isSearchable={false}
+                isDisabled={false}
+                isLoading={false}
+                isRtl={false}
+                menuIsOpen={isMenuOpen}
+                placeholder="Select the job position."
+              />
+              <label >
+                Email<br />
+                <input type="email" name="email" placeholder="johndoe@example.com" onChange={handlecandidateprofile} required /><br />
+              </label>
+              <label >
+                Phone<br />
+                <input type="phone" name="phone" placeholder="76968647" onChange={handlecandidateprofile} required /><br />
+              </label>
+
+              <label >
+                Profile Picture<br />
+                <input type="file" name="image" onChange={handlecandidateprofile} accept="image/*" /><br />
+              </label>
+            </fieldset>
+            <footer>
+              <button type="submit" onClick={handlecreatebtn}>Create</button>
+            </footer>
+          </form >
+          <Footer />
+        </>
+      )}
     </>
   );
 };
