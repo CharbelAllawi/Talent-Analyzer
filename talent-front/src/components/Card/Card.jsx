fc96b24e96f3c4ce4c8cdcaf9b614be7b4c08f8a
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-18 14:20:41 +0300

    Front <> Card Component

--- talent-front/src/components/Card/Card.jsx
+++ talent-front/src/components/Card/Card.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
 import './style.css';
+import { useNavigate } from 'react-router-dom';
+import Compare from '../Compare/Compare';
 
-const Card = ({ imageUrl, title }) => {
+const Card = ({ id, fullName, position, imageUrl, dob, phone, email, iscompared, comparepage }) => {
   const [isShowing, setIsShowing] = useState(false);
   const [zIndex, setZIndex] = useState(10);
+  const navigation = useNavigate();
 
 
 
-  const handleAdditionClick = (e) => {
-    e.stopPropagation();
-    // Implement the logic for the addition icon button here
-  };
 
-  const handleShowMoreClick = (e) => {
-    e.stopPropagation();
-    // Implement the logic for the show more icon button here
-  };
-
+  const handlEyeClick = (e) => {
+    navigation('/result/' + id)
+  }
   return (
     <>
       <div
@@ -25,35 +22,52 @@ const Card = ({ imageUrl, title }) => {
 
       >
         <div className="card__image-holder">
-          <img className="card__image" src={imageUrl} alt={title} />
+          <img className="card__image" src={"http://localhost:8000/storage/candidatesprofile/" + imageUrl} alt={fullName} />
         </div>
         <div className="card-title">
           <div className="button-container">
-            <button className="icon-button addition-button" onClick={handleAdditionClick}>
-              <i className="fas fa-user-plus" style={{ color: '#333366', fontSize: '24px', marginLeft: '12rem', marginRight: '1rem' }}></i> {/* Change color and size */}
-            </button>
-            <button className="icon-button show-more-button" onClick={handleShowMoreClick}>
-              <i className="fas fa-eye" style={{ color: '#333366', fontSize: '24px' }}></i> {/* Change color and size */}
-            </button>
+            {comparepage === false && (
+              <>
+                <button className="icon-button addition-button">
+                  <Compare iscompared={iscompared} candidate_id={id} />
+                </button>
+
+                <button className="icon-button show-more-button">
+                  <i className="fas fa-eye" onClick={handlEyeClick} style={{ color: '#9c9cd5', fontSize: '24px' }}></i>
+                </button>
+              </>
+            )}
           </div>
-          <h2>
-            {title}
-            <small>Image from unsplash.com</small>
-          </h2>
+          {comparepage === false && (
+            <h2>
+              {fullName}
+              <small>{position}</small>
+            </h2>
+          )}
+          {comparepage === true && (
+            <h2 style={{ marginTop: '0rem' }}>
+              {fullName}
+              <small>{position}</small>
+            </h2>
+          )}
         </div>
         <div className="card-flap flap1">
           <div className="card-description">
-            This grid is an attempt to make something nice that works on touch devices. Ignoring hover states when they're not available etc.
+            <ul className='uldesc'>
+              <li>Email : {email}</li>
+              <br></br>
+              <li>Phone : {phone}</li>
+              <br></br>
+              <li>DOB : {dob}</li>
+
+            </ul>
           </div>
           <div className="card-flap flap2">
             <div className="card-actions">
-              <a href="#" className="btn">
-                Read more
-              </a>
             </div>
           </div>
         </div>
-      </div>
+      </div >
     </>
 
   );
