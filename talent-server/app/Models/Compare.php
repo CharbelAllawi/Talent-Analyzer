84795d596645099076879fbe35a2dc9df131457d
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-18 14:17:27 +0300

    Back <> Compare Model

---
+++ talent-server/app/Models/Compare.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Compare extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // public function candidate()
+    // {
+    //     return $this->belongsTo(Candidate::class);
+    // }
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class, 'candidate_id', 'id');
+    }
+}
