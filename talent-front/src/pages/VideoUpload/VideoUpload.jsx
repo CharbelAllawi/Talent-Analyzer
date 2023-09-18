fde4de864d318b18893d7840831d832c2010c2de
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-18 14:18:48 +0300

    Front <> Video Upload

--- talent-front/src/pages/VideoUpload/VideoUpload.jsx
+++ talent-front/src/pages/VideoUpload/VideoUpload.jsx
@@ -7,7 +7,8 @@ import Navbar from '../../components/Navbar/Navbar';
 import { useParams } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Loading from '../Loading/Loading';
-
+import { sendRequest } from "../../core/config/request";
+import { requestMethods } from "../../core/enums/requestMethods";
 function VideoUpload() {
   const [uploadedFile, setUploadedFile] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -22,37 +23,29 @@ function VideoUpload() {
   };
 
   const uploadFile = (file) => {
-    setLoading(true);
-    if (!file || uploadedFile) return;
-
-    const filename =
-      file.name.length > 12 ? `${file.name.substring(0, 13)}... .${file.name.split('.')[1]}` : file.name;
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    setShowProgress(true);
-
-    axios
-      .post('http://127.0.0.1:8000/api/upload-file', formData, {
-        onUploadProgress: ({ loaded, total }) => {
-          if (loaded === total) {
-            const fileSize = total < 1024 ? `${total} KB` : `${(loaded / (1024 * 1024)).toFixed(2)} MB`;
-            setUploadedFile({ name: file.name, size: fileSize });
-            setShowProgress(false);
-            setLoading(false)
-          }
-        },
-      })
-
-      .catch(console.error);
+    setUploadedFile(file)
 
   };
-  const handlenextbtn = () => {
+  const handlenextbtn = async () => {
+    try {
+      navigation('/loading');
 
-    navigation('/select/' + id)
+      const formData = new FormData();
+      formData.append('file', uploadedFile);
+      formData.append('candidate_id', id);
 
+      const response = await sendRequest({
+        method: requestMethods.POST,
+        route: '/upload-file',
+        body: formData
+      });
+
+      navigation('/select/' + id);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
   const handleDelete = () => {
     setUploadedFile(null);
   };
@@ -69,59 +62,55 @@ function VideoUpload() {
 
   return (
     <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <Navbar selecteditem="Add Candidate" />
-          <div className='upload-box' onDrop={handleDrop} onDragOver={handleDragOver}>
-            <p>Upload Your Interview</p>
-            <form>
-              <input className='file-input' type='file' name='file' hidden ref={fileInputRef} onChange={(e) => uploadFile(e.target.files[0])}></input>
-              <div className='icon' onClick={handleFileInputClick}>
-                <img src={upload} alt='upload'></img>
-              </div>
-              <p>Browser File to upload</p>
-              <p className='text-xs'>File Type: MP4</p>
-            </form>
 
-            {showProgress && (
-              <section className='loading-area'>
-                <li className='row'>
-                  <i className='fas fa-file-alt'></i>
-                  <div className='content'>
-                    <div className='details'>
-                      <span className='name'>{`Uploading...`}</span>
-                    </div>
-                  </div>
-                </li>
-              </section>
-            )}
-
-            {uploadedFile && (
-              <section className='uploaded-area'>
-                <li className='row'>
-                  <div className='content upload'>
-                    <i className='fas fa-file-alt'></i>
-                    <div className='details'>
-                      <span className='name'>{uploadedFile.name}</span>
-                      <span className='size'>{uploadedFile.size}</span>
-                    </div>
-                  </div>
-                  <i className='fas fa-times' onClick={handleDelete} style={{ color: 'white', 'marginLeft': '45rem', 'position': 'absolute' }}></i>
-                </li>
-              </section>
-            )}
-            {uploadedFile && (
-              <span className="nextvideocontainer">
-                <button className="nextbuttonvideo" onClick={handlenextbtn}>Next</button>
-              </span>
-            )}
+      <Navbar selecteditem="Add Candidate" />
+      <div className='upload-box' onDrop={handleDrop} onDragOver={handleDragOver}>
+        <p>Upload Your Interview</p>
+        <form>
+          <input className='file-input' type='file' name='file' hidden ref={fileInputRef} onChange={(e) => uploadFile(e.target.files[0])}></input>
+          <div className='icon' onClick={handleFileInputClick}>
+            <img src={upload} alt='upload'></img>
           </div>
-          <Footer />
-        </>
-      )}
+          <p>Browser File to upload</p>
+          <p className='text-xs'>File Type: MP4</p>
+        </form>
+
+        {showProgress && (
+          <section className='loading-area'>
+            <li className='row'>
+              <i className='fas fa-file-alt'></i>
+              <div className='content'>
+                <div className='details'>
+                  <span className='name'>{`Uploading...`}</span>
+                </div>
+              </div>
+            </li>
+          </section>
+        )}
+
+        {uploadedFile && (
+          <section className='uploaded-area'>
+            <li className='row'>
+              <div className='content upload'>
+                <i className='fas fa-file-alt'></i>
+                <div className='details'>
+                  <span className='name'>{uploadedFile.name}</span>
+                  <span className='size'>{uploadedFile.size}</span>
+                </div>
+              </div>
+              <i className='fas fa-times' onClick={handleDelete} style={{ color: 'white', 'marginLeft': '45rem', 'position': 'absolute' }}></i>
+            </li>
+          </section>
+        )}
+        {uploadedFile && (
+          <span className="nextvideocontainer">
+            <button className="nextbuttonvideo" onClick={handlenextbtn}>Next</button>
+          </span>
+        )}
+      </div>
+      <Footer />
     </>
+
   );
 };
 
