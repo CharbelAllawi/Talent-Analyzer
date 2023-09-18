89344edee121d07f2b1ad520610141d94b4cfdbd
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-18 14:19:23 +0300

    Front <> MyCandidates Fixed

--- talent-front/src/pages/MyCandidates/MyCandidates.jsx
+++ talent-front/src/pages/MyCandidates/MyCandidates.jsx
@@ -1,60 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../components/Card/Card'
+import $ from 'jquery'; // Import jQuery
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import { sendRequest } from "../../core/config/request";
+import { requestMethods } from "../../core/enums/requestMethods";
+import { useNavigate } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
-const MyCandidates = () => {
-  const cardData = [
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?wave',
-      title: 'Card 1 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?beach',
-      title: 'Card 2 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?mountain',
-      title: 'Card 3 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?wave',
-      title: 'Card 1 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?beach',
-      title: 'Card 2 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?mountain',
-      title: 'Card 3 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?wave',
-      title: 'Card 1 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?beach',
-      title: 'Card 2 Title',
-    },
-    {
-      imageUrl: 'https://source.unsplash.com/300x225/?mountain',
-      title: 'Card 3 Title',
-    },
-    // Add more card data as needed
-  ];
+
+
+function MyCandidates() {
+  const navigation = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [cardData, setCardData] = useState([]);
+
+  const [zIndex, setZIndex] = useState(10);
+  const [isShowing, setIsShowing] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await sendRequest({
+          method: requestMethods.GET,
+          route: '/get-candidates',
+        });
+        setLoading(false);
+        console.log(response);
+
+        setCardData(response);
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
 
   return (
     <>
-      <Navbar selecteditem='My Candidates' />
-      <div className="cards">
-        {cardData.map((card, index) => (
-          <Card key={index} imageUrl={card.imageUrl} title={card.title} />
-        ))}
-      </div>
-      <Footer />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Navbar selecteditem='My Candidates' />
+          <div className="cards">
+            {cardData.map((candidate) => (
+              <Card
+                key={candidate.id}
+                id={candidate.id}
+                fullName={candidate.full_name}
+                position={candidate.position}
+                imageUrl={candidate.image_url}
+                dob={candidate.dob}
+                phone={candidate.phone_number}
+                email={candidate.email}
+                iscompared={candidate.iscompared}
+                comparepage={false}
+
+              />
+            ))}
+          </div>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
+
 
 export default MyCandidates;
