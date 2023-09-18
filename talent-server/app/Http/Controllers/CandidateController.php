a309d8bbef31699e4938ed07d976790b7e19fbf7
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-18 14:18:28 +0300

    Back <> Get Candidates

--- talent-server/app/Http/Controllers/CandidateController.php
+++ talent-server/app/Http/Controllers/CandidateController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use App\Models\Candidate;
 use App\Models\Result;
+use App\Models\User;
 use App\Models\UserCandidate;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
@@ -46,8 +47,54 @@ class CandidateController extends Controller
     public function getResult(Request $request)
     {
         $candidateId = $request->candidate_id;
+        $user = Auth::user();
 
-        $result = Result::where('candidate_id', $candidateId)->get();
-        return response()->json(['result' =>   $result]);
+        $authorizedCandidate = $user->candidates->first(function ($candidate) use ($candidateId) {
+            return $candidate->id == $candidateId;
+        });
+
+        if ($authorizedCandidate) {
+            $results = Result::where('candidate_id', $candidateId)->get();
+            return response()->json(['result' => $results]);
+        }
+
+        return response()->json(['result' => 'Not Authorized Or Not Found']);
+    }
+
+
+    public function getcandidates()
+    {
+        $user = Auth::user();
+
+        $candidates = $user->candidates->map(function ($candidate) {
+            $candidate = Candidate::find($candidate->id);
+            $compares = $candidate->compares;
+            if ($compares->count() > 0) {
+                return [
+                    'id' => $candidate->id,
+                    'full_name' => $candidate->full_name,
+                    'email' => $candidate->email,
+                    'phone_number' => $candidate->phone,
+                    'dob' => $candidate->date_of_birth,
+                    'position' => $candidate->position,
+                    'image_url' => $candidate->image_url,
+                    'iscompared' => true,
+                ];
+            } else {
+                return [
+                    'id' => $candidate->id,
+                    'full_name' => $candidate->full_name,
+                    'email' => $candidate->email,
+                    'phone_number' => $candidate->phone,
+                    'dob' => $candidate->date_of_birth,
+                    'position' => $candidate->position,
+                    'image_url' => $candidate->image_url,
+                    'iscompared' => false
+                ];
+            }
+        });
+
+
+        return $candidates;
     }
 }
