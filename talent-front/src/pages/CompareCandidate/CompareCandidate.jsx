b3e6b081b801b83e2e7dc61eec7892921810df59
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-22 17:39:25 +0300

    Front <> Compare Button

--- talent-front/src/pages/CompareCandidate/CompareCandidate.jsx
+++ talent-front/src/pages/CompareCandidate/CompareCandidate.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import Card from '../../components/Card/Card'
-import $ from 'jquery'; // Import jQuery
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import { sendRequest } from "../../core/config/request";
 import { requestMethods } from "../../core/enums/requestMethods";
 import { useNavigate } from 'react-router-dom';
 import Loading from '../Loading/Loading';
-
-
+import { CandidateCard } from '../../components/Card/Card';
+import Snackbar from '../../components/Snackbar/Snackbar';
+import "./style.css"
 
 function CompareCandidate() {
+
+  const SnackbarType = {
+    success: "success",
+    fail: "fail",
+  };
+
   const navigation = useNavigate();
+
   const [loading, setLoading] = useState(true);
   const [cardData, setCardData] = useState([]);
 
@@ -46,30 +52,55 @@ function CompareCandidate() {
         <Loading />
       ) : (
         <>
-          <Navbar selecteditem='Compare' />
-          <div className="cards">
-            {cardData.map((candidate) => (
-              candidate.iscompared === true ? (
-                <Card
-                  key={candidate.id}
-                  id={candidate.id}
-                  fullName={candidate.full_name}
-                  position={candidate.position}
-                  imageUrl={candidate.image_url}
-                  dob={candidate.dob}
-                  phone={candidate.phone_number}
-                  email={candidate.email}
-                  iscompared={candidate.iscompared}
-                  comparepage={true}
+          <Navbar selecteditem="Compare" />
+
+          <div className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ml-52 mt-20">
+            {cardData.length > 0 ? (
+              cardData.map((candidate) => (
+                candidate.iscompared ? (
+                  <CandidateCard
+                    key={candidate.id}
+                    id={candidate.id}
+                    fullName={candidate.full_name}
+                    position={candidate.position}
+                    imageUrl={candidate.image_url}
+                    dob={candidate.dob}
+                    phone={candidate.phone_number}
+                    email={candidate.email}
+                    iscompared={candidate.iscompared}
+                    comparepage={true}
+                  />
+                ) : null
+              ))
+            ) : (
+              <>
+                {navigation('/')}
+                <Snackbar
+
+                  message="Task Completed Successfully!"
+                  type={SnackbarType.success}
                 />
-              ) : null
-            ))}
+
+
+
+              </>
+
+
+            )}
+
           </div>
+          <span className="comparecontainer">
+            <button className="comparebutton">Compare</button>
+          </span>
+          <div className='footcont'></div>
+
           <Footer />
+
         </>
-      )}
+      )
+      }
     </>
   );
-}
 
+}
 export default CompareCandidate;
