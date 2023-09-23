46689df5933f1d248fb7b43e732a542b27bdbd92
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-23 13:34:07 +0300

    Back <> Compare Candidates

--- talent-server/app/Http/Controllers/CompareController.php
+++ talent-server/app/Http/Controllers/CompareController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Candidate;
 use App\Models\Compare;
 use Illuminate\Http\Request;
+use OpenAI;
 
 class CompareController extends Controller
 {
@@ -27,5 +28,25 @@ class CompareController extends Controller
         $existingcomprare = Compare::where('user_id', $user_id)->where('candidate_id', $request->candidate_id)->first();
         $existingcomprare->delete();
         return response()->json(['status' => 'UnCompared']);
+    }
+    public function comparecandidates(Request $request)
+    {
+        $yourApiKey = getenv('OPENAI_API_KEY');
+        $client = OpenAI::client($yourApiKey);
+        $jsonresult = $client->completions()->create([
+            'model' => 'gpt-3.5-turbo-instruct',
+            'prompt' => $request->prompt,
+            'max_tokens' => 2048,
+        ]);
+        $resultData = json_decode($jsonresult['choices'][0]['text'], true);
+        while (!isset($resultData['result'])) {
+            $jsonresult = $client->completions()->create([
+                'model' => 'gpt-3.5-turbo-instruct',
+                'prompt' => $request->prompt,
+                'max_tokens' => 2048,
+            ]);
+            $resultData = json_decode($jsonresult['choices'][0]['text'], true);
+        }
+        return $jsonresult['choices'][0]['text'];
     }
 }
