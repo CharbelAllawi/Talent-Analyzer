5b0784e6f04b7dd8018c400f630ab5dfba120ca8
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-23 13:35:50 +0300

    Front <> Compare Candidates Condition

--- talent-front/src/pages/CompareCandidate/CompareCandidate.jsx
+++ talent-front/src/pages/CompareCandidate/CompareCandidate.jsx
@@ -16,14 +16,38 @@ function CompareCandidate() {
     fail: "fail",
   };
 
+
   const navigation = useNavigate();
 
   const [loading, setLoading] = useState(true);
   const [cardData, setCardData] = useState([]);
 
-  const [zIndex, setZIndex] = useState(10);
-  const [isShowing, setIsShowing] = useState(false);
 
+  const handleCompareBtn = async () => {
+    let prompt = "i want you analyze the below candidates and return the chosen candidate\nreturn the answer as JSON parsable object (do not return any text or explanation or notes before or after the JSON object)\nalso make sure that the result inside the json object is only 5 lines maximum \nThe JSON object should be in this format {chosen_candidate :   , result:}"
+    cardData.forEach(data => {
+      if (data['iscompared'] == true) {
+        prompt += "\ncandidate_name:" + data['full_name']
+        prompt += "\nanalyzed result:" + data['result'] + "\n"
+      }
+    });
+    prompt += '\n(do not return any text or explanation or notes before or after the JSON object)'
+    try {
+      const response = await sendRequest({
+        method: requestMethods.POST,
+        route: '/comparecandidates',
+        body: { 'prompt': prompt }
+      });
+      localStorage.setItem("compareresult", response['result']);
+      localStorage.setItem("comparechose", response['chosen_candidate']);
+
+    } catch (error) {
+    }
+  }
+  const handleDelete = (idToDelete) => {
+    const updatedCardData = cardData.filter(candidate => candidate.id !== idToDelete);
+    setCardData(updatedCardData);
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -53,45 +77,38 @@ function CompareCandidate() {
       ) : (
         <>
           <Navbar selecteditem="Compare" />
-
+          <div className='hcontainer'>
+            <h1>Compare Candidates</h1>
+          </div>
           <div className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ml-52 mt-20">
-            {cardData.length > 0 ? (
-              cardData.map((candidate) => (
-                candidate.iscompared ? (
+
+            {cardData.map((candidate) => {
+
+              if (candidate.iscompared && cardData.filter((candidate) => candidate.iscompared).length >= 2) {
+                return (
                   <CandidateCard
                     key={candidate.id}
                     id={candidate.id}
-                    fullName={candidate.full_name}
+                    full_name={candidate.full_name}
                     position={candidate.position}
                     imageUrl={candidate.image_url}
-                    dob={candidate.dob}
+                    date_of_birth={candidate.dob}
                     phone={candidate.phone_number}
                     email={candidate.email}
                     iscompared={candidate.iscompared}
-                    comparepage={true}
+                    comparepage={false}
+                    onDelete={handleDelete}
                   />
-                ) : null
-              ))
-            ) : (
-              <>
-                {navigation('/')}
-                <Snackbar
-
-                  message="Task Completed Successfully!"
-                  type={SnackbarType.success}
-                />
-
-
-
-              </>
-
-
-            )}
-
+                );
+              }
+            })}
           </div>
           <span className="comparecontainer">
-            <button className="comparebutton">Compare</button>
+            {cardData.filter((candidate) => candidate.iscompared).length >= 2 && (
+              <button className="comparebutton" onClick={handleCompareBtn}>Compare</button>
+            )}
           </span>
+
           <div className='footcont'></div>
 
           <Footer />
--- talent-front/src/pages/Loading/Loading.jsx
+++ talent-front/src/pages/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css'; // Import your CSS file or style it inline
+import './style.css'; 
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 
