769440f241fdb4a20d4e63b604b559943329e560
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-25 16:33:33 +0300

    Front <> I18next Added To Footer

--- talent-front/src/components/Footer/Footer.jsx
+++ talent-front/src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./style.css"
 import logo from '../../assets/navlogo.svg'
+import { useTranslation } from 'react-i18next';
+
 function Footer() {
+  const [t, i18n] = useTranslation("global");
+
   return (
     <div class="pg-footer">
       <footer class="footer">
@@ -17,79 +21,79 @@ function Footer() {
               </a>
             </div>
             <div class="footer-menu">
-              <h2 class="footer-menu-name"> Get Started</h2>
+              <h2 class="footer-menu-name"> {t("footer.getstarted")}</h2>
               <ul id="menu-get-started" class="footer-menu-list">
                 <li class="menu-item menu-item-type-post_type menu-item-object-product">
-                  <a href="#">Start</a>
+                  <a href="#">{t("footer.start")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-product">
-                  <a href="#">Documentation</a>
+                  <a href="#">{t("footer.documentation")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-product">
-                  <a href="#">Installation</a>
+                  <a href="#">{t("footer.installation")}</a>
                 </li>
               </ul>
             </div>
           </div>
           <div class="footer-content-column">
             <div class="footer-menu">
-              <h2 class="footer-menu-name"> Company</h2>
+              <h2 class="footer-menu-name"> {t("footer.company")}</h2>
               <ul id="menu-company" class="footer-menu-list">
                 <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                  <a href="#">Contact</a>
+                  <a href="#">{t("footer.contact")}</a>
                 </li>
                 <li class="menu-item menu-item-type-taxonomy menu-item-object-category">
-                  <a href="#">News</a>
+                  <a href="#">{t("footer.news")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                  <a href="#">Careers</a>
+                  <a href="#">{t("footer.careers")}</a>
                 </li>
               </ul>
             </div>
             <div class="footer-menu">
-              <h2 class="footer-menu-name"> Legal</h2>
+              <h2 class="footer-menu-name"> {t("footer.legal")}</h2>
               <ul id="menu-legal" class="footer-menu-list">
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-170434">
-                  <a href="#">Privacy Notice</a>
+                  <a href="#">{t("footer.privacynotice")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                  <a href="#">Terms of Use</a>
+                  <a href="#">{t("footer.termsofuse")}</a>
                 </li>
               </ul>
             </div>
           </div>
           <div class="footer-content-column">
             <div class="footer-menu">
-              <h2 class="footer-menu-name"> Quick Links</h2>
+              <h2 class="footer-menu-name"> {t("footer.quicklink")}</h2>
               <ul id="menu-quick-links" class="footer-menu-list">
                 <li class="menu-item menu-item-type-custom menu-item-object-custom">
-                  <a target="_blank" rel="noopener noreferrer" href="#">Support Center</a>
+                  <a target="_blank" rel="noopener noreferrer" href="#">{t("footer.supportcenter")}</a>
                 </li>
                 <li class="menu-item menu-item-type-custom menu-item-object-custom">
-                  <a target="_blank" rel="noopener noreferrer" href="#">Service Status</a>
+                  <a target="_blank" rel="noopener noreferrer" href="#">{t("footer.servicestatus")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                  <a href="#">Security</a>
+                  <a href="#">{t("footer.security")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                  <a href="#">Blog</a>
+                  <a href="#">{t("footer.blog")}</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type_archive menu-item-object-customer">
-                  <a href="#">Customers</a></li>
+                  <a href="#">{t("footer.customers")}</a></li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                  <a href="#">Reviews</a>
+                  <a href="#">{t("footer.reviews")}</a>
                 </li>
               </ul>
             </div>
           </div>
           <div class="footer-content-column">
             <div class="footer-call-to-action">
-              <h2 class="footer-call-to-action-title"> Let's Chat</h2>
-              <p class="footer-call-to-action-description"> Have a support question?</p>
-              <a class="footer-call-to-action-button button" href="#" target="_self"> Get in Touch </a>
+              <h2 class="footer-call-to-action-title"> {t("footer.chat")}</h2>
+              <p class="footer-call-to-action-description"> {t("footer.support")}</p>
+              <a class="footer-call-to-action-button button" href="#" target="_self"> {t("footer.intouch")} </a>
             </div>
             <div class="footer-call-to-action">
-              <h2 class="footer-call-to-action-title"> You Call Us</h2>
+              <h2 class="footer-call-to-action-title"> {t("footer.callus")}</h2>
               <p class="footer-call-to-action-link-wrapper"> <a class="footer-call-to-action-link" href="tel:0124-64XXXX" target="_self"> 0124-64XXXX </a></p>
             </div>
           </div>
