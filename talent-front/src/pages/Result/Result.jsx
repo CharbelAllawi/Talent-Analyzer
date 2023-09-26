a893f321326d0c7db6ce3ed8757febe7528b5d57
CharbelAllawi <charbelallawiwork@gmail.com>
2023-09-27 00:51:57 +0300

    Front <> Chart Added

--- talent-front/src/pages/Result/Result.jsx
+++ talent-front/src/pages/Result/Result.jsx
@@ -9,18 +9,19 @@ import { requestMethods } from "../../core/enums/requestMethods";
 import { localStorageAction } from "../../core/config/localstorage";
 import Loading from '../Loading/Loading';
 import { useTranslation } from 'react-i18next';
+import Chart from 'chart.js/auto'; // Import Chart.js
 
 const Result = ({ content }) => {
   const [t, i18n] = useTranslation("global");
-
+  const [candidateskills, setCandidateSkills] = useState({});
   const navigation = useNavigate();
   const { id } = useParams();
   const [text, setText] = useState("");
   const [loading, setLoading] = useState(true);
   const [name, setName] = useState();
   const [resname, setResName] = useState();
-  useEffect(() => {
 
+  useEffect(() => {
     if (id !== 'compare') {
       const fetchData = async () => {
         try {
@@ -30,6 +31,9 @@ const Result = ({ content }) => {
             body: { 'candidate_id': id }
           });
           console.log(response);
+
+          const candidateSkillsObj = JSON.parse(response.result[0]['candidate_skills']);
+          setCandidateSkills(candidateSkillsObj);
           setText(response.result[0]['result']);
           setResName(response.result['full_name'])
           setLoading(false);
@@ -49,16 +53,53 @@ const Result = ({ content }) => {
         setText(compareResult);
         localStorage.removeItem('compareresult');
         setName(localStorage.getItem('comparechosen'));
+        const candidateSkillsObj = JSON.parse(localStorage.getItem('compareskills'));
+        setCandidateSkills(candidateSkillsObj);
         localStorage.removeItem('comparechosen');
+        localStorage.removeItem('compareskills');
       }
     }
   }, [id, navigation]);
 
-
   const handleFinishBtn = () => {
     navigation('/');
   }
+  useEffect(() => {
+    if (candidateskills) {
+      const ctx = document.getElementById('skillsChart');
+      if (ctx) {
+        if (window.myChart) {
+          window.myChart.destroy();
+        }
+        ctx.width = 600;
+        ctx.height = 400;
 
+        window.myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: Object.keys(candidateskills),
+            datasets: [
+              {
+                label: 'Skills',
+                data: Object.values(candidateskills),
+                backgroundColor: '#333366',
+                borderColor: '#333366',
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+                max: 100,
+              },
+            },
+          },
+        });
+      }
+    }
+  }, [candidateskills]);
   return (
     <>
       {loading ? (
@@ -71,6 +112,9 @@ const Result = ({ content }) => {
           </div>
           <div className="result-container">
             <textarea className="result-textarea" value={text} readOnly />
+            <div className="chart-container">
+              <canvas id="skillsChart"></canvas>
+            </div>
             <span className="finishcontainer">
               <button className="finishairesult" onClick={handleFinishBtn}>{t("airesult.finish")}</button>
             </span>
